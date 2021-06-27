0dc104b8d07aed6a8f3e9b241afb4490baff27d2
Brunno Faria <brunnofaria@gmail.com>
2021-06-27 18:41:57 -0300

    microdesafiomodulo

---
+++ PImicromodulos/moduloimport.js
@@ -0,0 +1,3 @@
+const exibirNomeCompleto = require("./modulosexportacao");
+
+exibirNomeCompleto(); 
---
+++ PImicromodulos/modulosexportacao.js
@@ -0,0 +1,6 @@
+function exibirNomeCompleto() {
+    let nomeCompleto = 'Brunno Alessandro Lima de Faria';
+    console.log(nomeCompleto);
+};
+
+module.exports = exibirNomeCompleto;
